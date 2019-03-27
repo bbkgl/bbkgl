7c2fce88d514175c10fca583c1b89a3984572ef7
Kunwuwu <heartless28@live.com>
2019-03-27 16:29:38 +0800

    初始化，这里已经拷贝了muduo的base库

---
+++ cmake-build-debug/CMakeFiles/bbkgl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bbkgl.dir/main.cpp.o"
+  "bbkgl.pdb"
+  "bbkgl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bbkgl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/bbkgl.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for bbkgl.
+# This may be replaced when dependencies are built.
