2b38e365f9f52c34f37a6d99b30098ce23af8dc3
Kunwuwu <heartless28@live.com>
2019-04-03 22:46:19 +0800

    solve

--- example/main.cpp
+++ example/main.cpp
@@ -7,26 +7,26 @@
 
 std::string msg;
 
-//void OnConnection(const TcpConnectionPtr &conn)
-//{
-//    if (conn->Connected())
-//    {
-//        printf("OnConnection(): {tid = %d} new connection [%s] from %s\n", std::this_thread::get_id(),
-//               conn->GetName().c_str(),
-//               conn->GetPeerAddress().ToHostPort().c_str());
-//        conn->Send(msg);
-//    } else
-//    {
-//        printf("OnConnection(): {tid = %d} connection [%s] is down\n", std::this_thread::get_id(),
-//               conn->GetName().c_str());
-//    }
-//}
-//
-//void OnWriteComplete(const TcpConnectionPtr &conn)
-//{
-//    conn->Send(msg);
-//    printf(" {tid = %d} OnWriteComplete\n", std::this_thread::get_id());
-//}
+void OnConnection(const TcpConnectionPtr &conn)
+{
+    if (conn->Connected())
+    {
+        printf("OnConnection(): {tid = %d} new connection [%s] from %s\n", std::this_thread::get_id(),
+               conn->GetName().c_str(),
+               conn->GetPeerAddress().ToHostPort().c_str());
+        conn->Send(msg);
+    } else
+    {
+        printf("OnConnection(): {tid = %d} connection [%s] is down\n", std::this_thread::get_id(),
+               conn->GetName().c_str());
+    }
+}
+
+void OnWriteComplete(const TcpConnectionPtr &conn)
+{
+    conn->Send(msg);
+    printf(" {tid = %d} OnWriteComplete\n", std::this_thread::get_id());
+}
 
 /*
  * 这个write_complete__callback_ = nullptr;非常重要！！！！！！
@@ -38,41 +38,41 @@ std::string msg;
  *
  * */
 
-//void OnMessage(const TcpConnectionPtr &conn,
-//               Buffer *data,
-//               Timestamp recv_time)
-//{
-//    std::string recv = data->RetrieveAsString();
-//    printf("OnMessage(): {tid = %d} received %zd bytes from connection [%s] at %s\n", std::this_thread::get_id(),
-//           data->ReadableBytes(), conn->GetName().c_str(), recv_time.toFormattedString().c_str());
-//    printf("OnMessage: {tid = %d} [%s]\n", std::this_thread::get_id(), recv.substr(0, recv.length() - 1).c_str());
-//}
-//
-//int main()
-//{
-//    printf("test11: pid = %d\n", getpid());
-//
-//    std::string line;
-//
-//    for (int i = 33; i < 127; i++)
-//        line.push_back(static_cast<char>(i));
-//
-//    line += line;
-//
-//    for (size_t i = 0; i < 127-33; ++i)
-//        msg += line.substr(i, 72) + "\n";
-//
-//    InetAddress listen_addr(2333);
-//    EventLoop loop;
-//
-//    TcpServer server(&loop, listen_addr);
-//    server.SetConnCallback(OnConnection);
-//    server.SetMsgCallback(OnMessage);
-//    server.SetWriteCompleteCallback(OnWriteComplete);
-//    server.SetThreadNum(4);
-//    server.Start();
-//
-//    loop.Loop();
-//
-//    return 0;
-//}
+void OnMessage(const TcpConnectionPtr &conn,
+               Buffer *data,
+               Timestamp recv_time)
+{
+    std::string recv = data->RetrieveAsString();
+    printf("OnMessage(): {tid = %d} received %zd bytes from connection [%s] at %s\n", std::this_thread::get_id(),
+           data->ReadableBytes(), conn->GetName().c_str(), recv_time.toFormattedString().c_str());
+    printf("OnMessage: {tid = %d} [%s]\n", std::this_thread::get_id(), recv.substr(0, recv.length() - 1).c_str());
+}
+
+int main()
+{
+    printf("test11: pid = %d\n", getpid());
+
+    std::string line;
+
+    for (int i = 33; i < 127; i++)
+        line.push_back(static_cast<char>(i));
+
+    line += line;
+
+    for (size_t i = 0; i < 127-33; ++i)
+        msg += line.substr(i, 72) + "\n";
+
+    InetAddress listen_addr(2333);
+    EventLoop loop;
+
+    TcpServer server(&loop, listen_addr);
+    server.SetConnCallback(OnConnection);
+    server.SetMsgCallback(OnMessage);
+    server.SetWriteCompleteCallback(OnWriteComplete);
+    server.SetThreadNum(4);
+    server.Start();
+
+    loop.Loop();
+
+    return 0;
+}
